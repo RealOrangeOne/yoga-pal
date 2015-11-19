eff7d1fc02df9a3c6240ab4358abe260d6b3dac1
Jake Howard <git@theorangeone.net>
2015-11-19 18:29:40 +0000

    Added rotation

--- project/rotate/cli.py
+++ project/rotate/cli.py
@@ -1,11 +1,32 @@
 import click
 import os
 
+SCREEN_COMMAND = "xrandr -o {}"
+SCREEN_FLIP = "inverted"
+SCREEN_DEFAULT = "normal"
 
-@click.command('rotate', short_help='Shows file changes.')
-@click.arguement('component', default='all', type=click.Choices(['screen', 'touch', 'all']))
+TOUCH_COMMAND = "xinput set-prop 'ELAN Touchscreen' 'Coordinate Transformation Matrix' {}"
+TOUCH_FLIP = "-1 0 1 0 -1 1 0 0 1"
+TOUCH_DEFAULT = "1 0 0 0 1 0 0 0 1"
+
+@click.command('rotate', short_help='Rotate components.')
+@click.argument('component', default='all', type=click.Choice(['screen', 'touch', 'all']))
 @click.option('--flip/--default', default=None)
 def cli(component, flip):
-    pass
+    screen_addon = SCREEN_DEFAULT if not flip else SCREEN_FLIP
+    touch_addon = TOUCH_DEFAULT if not flip else TOUCH_FLIP
+
+    SCREEN = SCREEN_COMMAND.format(screen_addon)
+    TOUCH = TOUCH_COMMAND.format(touch_addon)
+
+    exit_code = 0
+    if component in ['screen', 'all']:
+        print("Rotating Screen...")
+        exit_code = os.system(SCREEN)
+    if exit_code == 0 and component in ['touch', 'all']:
+        print("Rotating Touch Screen...")
+        exit_code = os.system(TOUCH)
+
+    return exit_code
 
     
