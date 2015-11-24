b35b310bd8f4856cbbb15ff7cdebe0df1c5ab2b9
Jake Howard <git@theorangeone.net>
2015-11-24 18:53:27 +0000

    Added touch screen disable

--- project/disable/cli.py
+++ project/disable/cli.py
@@ -2,12 +2,19 @@ import click
 import os
 
 TRACKPAD_COMMAND = "xinput {} 'SynPS/2 Synaptics TouchPad'"
+TOUCH_COMMAND = "xinput {} 'ELAN Touchscreen'"
 
 @click.command('disable', short_help='Disable component.')
 @click.argument('component', default='all', type=click.Choice(['trackpad', 'touch', 'all']))
 @click.option('--enable/--disable', default=None)
 def cli(component, enable):
+    exit_code = 0
+    mode = "enable" if enable else "disable"
+    print("Mode", mode)
     if component in ['trackpad', 'all']:
-        exit_code = 0
-        mode = "enable" if enable else "disable"
         exit_code = os.system(TRACKPAD_COMMAND.format(mode))
+
+    if exit_code != 0 and component in ['touch', 'all']:
+        exit_code = os.system(TOUCH_COMMAND.format(mode))
+
+    return exit_code
