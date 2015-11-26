2d185a9d9a64a886df586fa553e28cc86d789c50
Jake Howard <git@theorangeone.net>
2015-11-26 18:52:28 +0000

    Updated modes

--- project/mode/cli.py
+++ project/mode/cli.py
@@ -1,6 +1,7 @@
 import click
 from project.rotate.cli import cli as rotate_cli
 from project.disable.cli import cli as disable_cli
+from project.enable.cli import cli as enable_cli
 
 
 MODES = ['laptop', 'tablet']
@@ -16,7 +17,7 @@ def cli():
 def tablet(ctx):
     exit_code = ctx.invoke(rotate_cli, component='all', flip=True)
     if exit_code == 0:
-        exit_code = ctx.invoke(disable_cli, component='trackpad', enable=False)
+        exit_code = ctx.invoke(disable_cli, component='trackpad')
     return exit_code
 
 
@@ -25,7 +26,7 @@ def tablet(ctx):
 def laptop(ctx):
     exit_code = ctx.invoke(rotate_cli, component='all', flip=False)
     if exit_code == 0:
-        exit_code = ctx.invoke(disable_cli, component='all', enable=True)
+        exit_code = ctx.invoke(enable_cli, component='all')
     return exit_code
 
 
