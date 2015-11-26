56e8ae351dbb57089e3d7c4d96e5f2fad3a526ce
Jake Howard <git@theorangeone.net>
2015-11-26 09:12:29 +0000

    Added more modes

--- project/mode/cli.py
+++ project/mode/cli.py
@@ -2,13 +2,25 @@ import click
 from project.rotate.cli import cli as rotate_cli
 from project.disable.cli import cli as disable_cli
 
-@click.group('mode', short_help='Rotate components.')
-def cli():
+
+@click.group()
+def cli(ctx):
     pass
 
+
+@cli.command()
 @click.pass_context
 def tablet(ctx):
     exit_code = ctx.invoke(rotate_cli, component='all', flip=True)
-    if exit_code != 0:
+    if exit_code == 0:
         exit_code = ctx.invoke(disable_cli, component='trackpad', enable=False)
+    return exit_code
+
+
+@cli.command
+@click.pass_context
+def laptop(ctx):
+    exit_code = ctx.invoke(rotate_cli, component='all', flip=False)
+    if exit_code == 0:
+        exit_code = ctx.invoke(disable_cli, component='all', enable=True)
     return exit_code
